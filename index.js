c6d711de6c802bc6fd62962f609cac8b0ecc779d
Kevin Ngo <k.ngo93@outlook.com>
2018-12-28 19:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+
+  for (var x = 0; x < musicians.length; x++) {
+    array.push(musicians[x] + "plays " + instruments[x]);
+  }
+
+  return array;
+}
